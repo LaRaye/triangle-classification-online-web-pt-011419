fecee114164ce4f6be090459d68bca84264f0b4a
LaRaye Johnson <lnj2105@gmail.com>
2019-02-17 20:31:34 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,19 +1,28 @@
 class Triangle
-  attr_accessor :l_1, :l_2, :l_3
+  attr_accessor :sidea, :sideb, :sidec
   
-  def initialize(l_1, l_2, l_3)
-    @l_1 = l_1
-    @l_2 = l_2
-    @l_3 = l_3
-  end 
-  
-  def kind
-    sides = []
-    sides << l_1 << l_2 << l_3
-    
-    
-    
-    
-    
-  end 
+  def initialize (sidea, sideb, sidec)
+    @sidea= sidea
+    @sideb=sideb
+    @sidec=sidec
+  end
+   
+  def kind 
+    if sidea <=0 || sideb <= 0 || sidec <= 0 || (sidea + sideb) <= sidec || (sidea+ sidec) <= sideb || (sideb+sidec) <= sidea
+      raise TriangleError
+    elsif  sidea == sideb && sidea == sidec 
+      :equilateral
+    elsif sideb== sidec && sideb != sidea && sidec !=sidea
+      :isosceles
+    elsif sidea == sideb && sidea != sidec && sideb !=sidec
+      :isosceles
+    elsif sidea== sidec && sideb !=sidea && sidec != sideb
+       :isosceles
+    else
+      :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+  end
 end
